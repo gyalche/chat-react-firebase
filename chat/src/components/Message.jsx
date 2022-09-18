e5d9f08af24f755384c5138e292b284a2133b997
Dawa <sherpadawa01@outlook.com>
2022-09-19 01:01:30 +0545

    woking on flex

--- chat/src/components/Message.jsx
+++ chat/src/components/Message.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import profile from '../img/pm.png';
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className='messageInfo'>
         <img src={profile} alt='' />
         <span>just now</span>
       </div>
       <div className='messageContent'>
         <p>Hellow</p>
+        <img src={profile} alt='' />
       </div>
     </div>
   );
