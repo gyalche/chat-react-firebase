348bfc0a6fe191d2c8ae1741f89e5f6a7064a2ca
Dawa <sherpadawa01@outlook.com>
2022-09-20 13:03:18 +0545

    final touch

--- chat/src/components/Chats.jsx
+++ chat/src/components/Chats.jsx
@@ -25,18 +25,20 @@ const Chats = () => {
   };
   return (
     <div className='chats'>
-      {Object.entries(chats)?sort((a,b)=>b[1].date-a[1].date).map((chat) => (
-        <div
-          className='userChat'
-          key={chat[0]}
-          onClick={() => handleSelect(chat[1].userInfo)}>
-          <img src={chat[1].userinfo?.photoURL} alt='' />
-          <div className='userChatInfo'>
-            <span>{chat[1].userinfo?.displayName}</span>
-            <p>{chat[1].userinfo?.lastMessage?.text}</p>
+      {Object.entries(chats)
+        ?.sort((a, b) => b[1].date - a[1].date)
+        .map((chat) => (
+          <div
+            className='userChat'
+            key={chat[0]}
+            onClick={() => handleSelect(chat[1].userInfo)}>
+            <img src={chat[1].userinfo?.photoURL} alt='' />
+            <div className='userChatInfo'>
+              <span>{chat[1].userinfo?.displayName}</span>
+              <p>{chat[1].userinfo?.lastMessage?.text}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
