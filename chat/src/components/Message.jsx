6031b1560cad05e88344a560b711e3fe045e376f
Dawa <sherpadawa01@outlook.com>
2022-09-20 12:59:22 +0545

    work done for  chat application

--- chat/src/components/Message.jsx
+++ chat/src/components/Message.jsx
@@ -1,20 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/chatContext';
 import profile from '../img/pm.png';
 const Message = ({ message }) => {
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [message]);
   console.log(message);
   return (
-    <div className='message owner'>
+    <div
+      className={`message ${message.senderId === currentUser.uid && 'owner'}`}>
       <div className='messageInfo'>
-        <img src={profile} alt='' />
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoUrl
+          }
+          alt=''
+        />
         <span>just now</span>
       </div>
       <div className='messageContent'>
-        <p>Hellow</p>
-        <img src={profile} alt='' />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt='' />}
       </div>
     </div>
   );
